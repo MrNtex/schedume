ccaeb773078da8d08e591f04f1a738d013095619
MrNtex <ntexyt@gmail.com>
2024-08-26 16:21:44 +0200

    chore: Update DayCalendar component to improve event functionality

--- schedume/components/CalendarEvent.tsx
+++ schedume/components/CalendarEvent.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function CalendarEvent({eventId}: {eventId:number}) {
   return (
-    <div key={eventId} className='bg-emerald-500 rounded-xl px-5 top-2'>
+    <div key={eventId} className='bg-emerald-500 rounded-xl px-5 top-2' style={{
+      top: `${1300 / (24 * 60) * 100}%`,
+
+    }}>
         <h1 className='text-white text-3xl font-bold p-4'>Event</h1>
         <p className='text-white text-lg p-4'>Event details</p>
     </div>
--- schedume/components/DayCalendar.tsx
+++ schedume/components/DayCalendar.tsx
@@ -29,27 +29,23 @@ export default function DayCalendar() {
   };
 
   const Hour = ({ hour }: { hour: string }) => {
-    return <div>
-        <div className="py-3 pb-10 px-5 flex items-center text-sm text-gray-800 before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-white dark:before:border-neutral-600 dark:after:border-neutral-600">
-          <div className="hover:bg-emerald-500 p-2 rounded-sm ease-in-out transition duration-300 cursor-pointer" onClick={() => addEvent(hour)}>{hour}</div>
-        </div>
-        {events
-          .filter((event) => event.hour === hour)
-          .map((event, idx) => (
-            <CalendarEvent eventId={idx} />
-          ))}
-      </div>
+    return (
+        <div className="px-5 pb-12 flex items-center justify-center text-sm text-gray-800 before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-white dark:before:border-neutral-600 dark:after:border-neutral-600">
+          <div className="hover:bg-emerald-500 rounded-sm ease-in-out transition duration-300 cursor-pointer absolute" onClick={() => addEvent(hour)}>{hour}</div>
+        
+      </div>)
   }
 
   return (
-    <div className='relative flex justify-center items-center'>
-      <div className="w-[60%]">
+    <div className='relative flex justify-center items-center py-5'>
+      <div className=" relative w-[60%]">
         {/* Current Time Line */}
         {currentTime && (
           <div
             className="absolute left-0 right-0 h-0.5 bg-red-500 w-[70%] mx-auto"
             style={{
-              top: `${(new Date().getHours() * 60 + new Date().getMinutes()) / 1400 * 100}%`,
+              top: `${(new Date().getHours() * 60 + new Date().getMinutes()) / (24 * 60) * 100}%`,
+
             }}
           >
             <span className="absolute left-0 -translate-y-1/2 -translate-x-12 bg-red-500 text-white px-2 py-1 rounded">
@@ -62,7 +58,14 @@ export default function DayCalendar() {
             <Hour hour={hour}/>
           ))}
         </div>
+        <div className="absolute right-0 top-0 w-[30%]">
+          {events
+          .map((event, idx) => (
+            <CalendarEvent eventId={idx} />
+          ))}
+        </div>
       </div>
+      
     </div>
     
   );
