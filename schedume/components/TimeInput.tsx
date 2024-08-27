c10dba0ca1088940d48612ddd69bb8623b87ee43
MrNtex <ntexyt@gmail.com>
2024-08-27 12:16:05 +0200

    refactor: Update TimeInput component to improve time selection functionality

--- schedume/components/TimeInput.tsx
+++ schedume/components/TimeInput.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { Input } from './ui/input';
 import { EventType } from '@/context/ScheduleContext';
+import { Button } from './ui/button';
+import { MinusIcon, PlusIcon } from 'lucide-react';
 
 export default function TimeInput({ event }: { event?: EventType }) 
 { // Update the type of the event parameter
-  let defaultHour = '00';
+  let defaultHour = '';
   if(event) defaultHour = event.hour < 10 ? `0${event.hour}` : event.hour.toString();
   const [hour, setHour] = useState(defaultHour);
-  const [minute, setMinute] = useState('00');
+  const [minute, setMinute] = useState('');
 
   const handleHourChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let value = e.target.value.replace(/\D/g, ''); // Remove non-numeric characters
@@ -36,39 +38,88 @@ export default function TimeInput({ event }: { event?: EventType })
     setMinute(value);
   };
 
-  const increaseHour = () => {
+  const changeHour = (amount: number) => {
     setHour((prev) => {
-      const newHour = Math.min(Number(prev) + 1, 24);
+      let newHour = (Number(prev) + amount) % 24;
+      if (newHour < 0) newHour = 23
       return newHour < 10 ? `0${newHour}` : newHour.toString();
     });
   };
 
-  const decreaseHour = () => {
-    setHour((prev) => {
-      const newHour = Math.max(Number(prev) - 1, 0);
-      return newHour < 10 ? `0${newHour}` : newHour.toString();
+  const changeMinute = (amount: number) => {
+    setMinute((prev) => {
+        let newMinute = Math.floor((Number(prev) + amount) / 15) * 15;
+        newMinute = newMinute % 60;
+        if (newMinute < 0) newMinute = 60 + newMinute;
+        return newMinute < 10 ? `0${newMinute}` : newMinute.toString();
     });
   };
 
   return (
     <div className="flex items-center">
-      <button type="button" onClick={decreaseHour}>-</button>
+      <div className="flex items-center gap-2 relative w-24">
+      <Button
+        type='button'
+        variant="outline"
+        size="icon"
+        className="h-5 w-5 shrink-0 rounded-full absolute left-2"
+        onClick={() => changeHour(-1)}
+        >
+        <MinusIcon className="h-4 w-4" />
+        <span className="sr-only">Decrease</span>
+      </Button>
       <Input
         id="timeHour"
+        className="w-full text-center pl-2 pr-2" 
         value={hour}
         onChange={handleHourChange}
         maxLength={2}
-        placeholder="HH"
+        placeholder="00"
       />
-      <button type="button" onClick={increaseHour}>+</button>
+      <Button
+        type='button'
+        variant="outline"
+        size="icon"
+        className="h-5 w-5 shrink-0 rounded-full absolute right-2"
+        onClick={() => changeHour(1)}
+        >
+        <PlusIcon className="h-4 w-4" />
+        <span className="sr-only">Increase</span>
+      </Button>
+      </div>
       <span className="mx-2">:</span>
+      <div className="flex items-center gap-2 relative w-24">
+      <Button
+        type='button'
+        variant="outline"
+        size="icon"
+        className="h-5 w-5 shrink-0 rounded-full absolute left-2"
+        onClick={() => changeMinute(-15)}
+        >
+        <MinusIcon className="h-4 w-4" />
+        <span className="sr-only">Decrease</span>
+      </Button>
       <Input
         id="timeMinute"
+        className="w-full text-center pl-2 pr-2" // Adjust padding for button spac
         value={minute}
         onChange={handleMinuteChange}
         maxLength={2}
-        placeholder="MM"
+        placeholder="00"
       />
+      <Button
+        type='button'
+        variant="outline"
+        size="icon"
+        className="h-5 w-5 shrink-0 rounded-full absolute right-2"
+        onClick={() => changeMinute(15)}
+        >
+        <PlusIcon className="h-4 w-4" />
+        <span className="sr-only">Increase</span>
+      </Button>
+      </div>
+      
+      
     </div>
   );
 }
